dd7c4a939c4b6d250ba54c0aa9b718dc3b877958
joao <morte901@gmail.com>
2025-03-21 16:54:12 -0300

    criando projeto

---
+++ aulacidade/Repositorio/Conexao.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace aulacidade.Repositorio
+{
+    
+        public class Conexao : IDisposable
+        {
+
+            private MySqlConnection _connection;
+
+            public Conexao(string connectionString)
+            {
+                _connection = new MySqlConnection(connectionString);
+                _connection.Open();
+            }
+
+            public MySqlCommand MySqlCommand()
+            {
+                return _connection.CreateCommand();
+            }
+
+            public void Dispose()
+            {
+                if (_connection != null && _connection.State == ConnectionState.Open)
+                {
+                    _connection.Close();
+                    _connection.Dispose();
+                }
+            }
+        }
+
+    }
+
+
